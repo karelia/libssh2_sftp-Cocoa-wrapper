6d6f544ae7bc2f713849c2775ca3f56f5c964499
Uri Baghin <skerfat@gmail.com>
2013-02-20 04:55:16 +0100

    Change ARCHS not to be hardcoded.

--- build-openssl-darwin.sh
+++ build-openssl-darwin.sh
@@ -1,36 +1,35 @@
 # Break out if the lib already exists.
-if [ -e "${CONFIGURATION_TEMP_DIR}/libcrypto.dylib" ] && [ -e "${CONFIGURATION_TEMP_DIR}/libssl.dylib" ]
+if [ -e "${TARGET_TEMP_DIR}/libcrypto.dylib" ] && [ -e "${TARGET_TEMP_DIR}/libssl.dylib" ]
 then
 exit 0
 fi
 
 # Include default paths for MacPorts & HomeBrew.
 export PATH=${PATH}:/opt/local/bin:/usr/local/bin
-# Force building for both archs so we don't have to worry about Xcode magic.
-ARCHS="i386 x86_64"
+
 ARCH_WORKING_DIR_PREFIX="${TARGET_TEMP_DIR}/${TARGET_NAME}/${TARGET_NAME}"
+LIBCRYPTO_LIPO_ARGS=()
+LIBSSL_LIPO_ARGS=()
 
 for ARCH in ${ARCHS}
 do
 
-ARCH_WORKING_DIR="${ARCH_WORKING_DIR_PREFIX}-${ARCH}"
-if [ "${ARCH}" == "i386" ];
-then
-ARCH_TRIPLE="darwin-i386-cc"
-else
-ARCH_TRIPLE="darwin64-x86_64-cc"
-fi
-
-
 # Copy source to a new location to build.
+ARCH_WORKING_DIR="${ARCH_WORKING_DIR_PREFIX}-${ARCH}"
 cd "${SRCROOT}"
 mkdir -p "${ARCH_WORKING_DIR}"
 cp -af openssl/ "${ARCH_WORKING_DIR}"
 cd "${ARCH_WORKING_DIR}"
 
 # Configure & build
-export CC="clang -isysroot ${SDKROOT} -g -w -mmacosx-version-min=10.6    "
-./Configure --prefix="@loader_path/../Frameworks"  threads shared zlib-dynamic  no-krb5 no-jpake no-store  "${ARCH_TRIPLE}"
+if [ "${ARCH}" == "i386" ]
+then
+ARCH_TRIPLE="darwin-i386-cc"
+else
+ARCH_TRIPLE="darwin64-x86_64-cc"
+fi
+export CC="clang -isysroot ${SDKROOT} -g -w -mmacosx-version-min=10.6"
+./Configure --prefix="@loader_path/../Frameworks" threads shared zlib-dynamic no-krb5 no-jpake no-store "${ARCH_TRIPLE}"
 make build_libs
 
 # Correct the load commands of the dylibs.
@@ -49,6 +48,10 @@ install_name_tool -add_rpath @loader_path/../Frameworks ${LONG_SSL_DYLIB}
 cp -f ${LONG_CRYPTO_DYLIB} libcrypto-${ARCH}.dylib
 cp -f ${LONG_SSL_DYLIB} libssl-${ARCH}.dylib
 
+# Add to the lipo command.
+LIBCRYPTO_LIPO_ARGS=("${LIBCRYPTO_LIPO_ARGS[@]}" "-arch" "${ARCH}" "${ARCH_WORKING_DIR}/libcrypto-${ARCH}.dylib")
+LIBSSL_LIPO_ARGS=("${LIBSSL_LIPO_ARGS[@]}" "-arch" "${ARCH}" "${ARCH_WORKING_DIR}/libssl-${ARCH}.dylib")
+
 # Preserve .a files name & location. They are needed by dsymutil later.
 # libcrypto.a, libssl.a
 
@@ -56,10 +59,9 @@ done
 
 
 # Create final dylib.
-
-cd "${CONFIGURATION_TEMP_DIR}"
-lipo -create -arch i386 "${ARCH_WORKING_DIR_PREFIX}-i386/libcrypto-i386.dylib" -arch x86_64 "${ARCH_WORKING_DIR_PREFIX}-x86_64/libcrypto-x86_64.dylib" -output libcrypto.dylib
-lipo -create -arch i386 "${ARCH_WORKING_DIR_PREFIX}-i386/libssl-i386.dylib"    -arch x86_64 "${ARCH_WORKING_DIR_PREFIX}-x86_64/libssl-x86_64.dylib"    -output libssl.dylib
+cd "${TARGET_TEMP_DIR}"
+lipo -create "${LIBCRYPTO_LIPO_ARGS[@]}" -output libcrypto.dylib
+lipo -create "${LIBSSL_LIPO_ARGS[@]}" -output libssl.dylib
 
 
 # Create dSYM
--- build-openssl-iphoneos.sh
+++ build-openssl-iphoneos.sh
@@ -1,32 +1,69 @@
+# Original script by Felix Shulze https://github.com/x2on/libssh2-for-iOS
+
+# Break out if the lib already exists.
+if [ -e "${CONFIGURATION_TEMP_DIR}/libcrypto-iOS.dylib" ] && [ -e "${CONFIGURATION_TEMP_DIR}/libssl-iOS.dylib" ]
+then
+exit 0
+fi
+
 # Include default paths for MacPorts & HomeBrew.
 export PATH=${PATH}:/opt/local/bin:/usr/local/bin
 
+ARCH_WORKING_DIR_PREFIX="${TARGET_TEMP_DIR}/${TARGET_NAME}/${TARGET_NAME}"
+LIBCRYPTO_LIPO_ARGS=()
+LIBSSL_LIPO_ARGS=()
 
 for ARCH in ${ARCHS}
 do
-if [ "${ARCH}" == "i386" ];
+
+# Copy source to a new location to build.
+ARCH_WORKING_DIR="${ARCH_WORKING_DIR_PREFIX}-${ARCH}"
+cd "${SRCROOT}"
+mkdir -p "${ARCH_WORKING_DIR}"
+cp -af libssh2/ "${ARCH_WORKING_DIR}"
+cd "${ARCH_WORKING_DIR}"
+
+if [ ! "${ARCH}" == "i386" ]
 then
-PLATFORM="iPhoneSimulator"
-else
 sed -ie "s!static volatile sig_atomic_t intr_signal;!static volatile intr_signal;!" "crypto/ui/ui_openssl.c"
-PLATFORM="iPhoneOS"
 fi
 
-export CROSS_TOP="${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer"
-export CROSS_SDK="${PLATFORM}${SDKVERSION}.sdk"
+export CC="clang -arch ${ARCH} -isysroot ${SDKROOT} -g -w"
 
-echo "Building openssl-${VERSION} for ${PLATFORM} ${SDKVERSION} ${ARCH}"
-echo "Please stand by..."
-
-export CC="${CROSS_TOP}/usr/bin/gcc -arch ${ARCH}"
-mkdir -p "${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk"
-LOG="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk/build-openssl-${VERSION}.log"
-
-./Configure iphoneos-cross --openssldir="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk" > "${LOG}" 2>&1
+./Configure iphoneos-cross
 # add -isysroot to CC=
-sed -ie "s!^CFLAG=!CFLAG=-isysroot ${CROSS_TOP}/SDKs/${CROSS_SDK} !" "Makefile"
+sed -ie "s!^CFLAG=!CFLAG=-isysroot ${SDKROOT} !" "Makefile"
 
-make >> "${LOG}" 2>&1
-make install >> "${LOG}" 2>&1
-make clean >> "${LOG}" 2>&1
+make build_libs
+
+# Copy libraries to have arch in name.
+cp -f libcrypto.a libcrypto-${ARCH}.a
+cp -f libssl.a libssl-${ARCH}.a
+
+# Add to the lipo command.
+LIBCRYPTO_LIPO_ARGS=("${LIBCRYPTO_LIPO_ARGS[@]}" "-arch" "${ARCH}" "${ARCH_WORKING_DIR}/libcrypto-${ARCH}.a")
+LIBSSL_LIPO_ARGS=("${LIBSSL_LIPO_ARGS[@]}" "-arch" "${ARCH}" "${ARCH_WORKING_DIR}/libssl-${ARCH}.a")
+
+# Preserve .a files name & location. They are needed by dsymutil later.
+# libcrypto.a, libssl.a
+
 done
+
+# Create final library.
+cd "${CONFIGURATION_TEMP_DIR}"
+lipo -create "${LIBCRYPTO_LIPO_ARGS[@]}" -output libcrypto-iOS.a
+lipo -create "${LIBSSL_LIPO_ARGS[@]}" -output libssl-iOS.a
+
+
+# Create dSYM
+# NOTE: dsymutil depends on the static libraries being in the same place and having the same name (see previous note)
+dsymutil libcrypto-iOS.a
+dsymutil libssl-iOS.a
+
+# Strip dylib
+strip -x libcrypto-iOS.a
+strip -x libssl-iOS.a
+
+# Copy headers
+mkdir -p include
+cp -fRL "${ARCH_WORKING_DIR}/include/" include
